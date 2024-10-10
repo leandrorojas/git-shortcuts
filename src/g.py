a8c4ac29433edeef238d739fe726cdda3295b725
Leandro Rojas <lrojas@tecnoazar.com.ar>
2024-10-10 14:56:57 -0300

    added git_branch fn

---
+++ src/g.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# output = subprocess.run(["git", "pull"], capture_output=True)
+# print(f"{output.stdout.decode()}")
+
+GIT_COMMAND = "git"
+GIT_BRANCH = "branch"
+
+def git_branch(branch_name:str) -> str:
+    result =  subprocess.run([GIT_COMMAND, GIT_BRANCH, branch_name], capture_output=True)
+    if result.stderr.decode() != "": return result.stderr.decode()
+    return result.stdout.decode()
+
+
+print(git_branch("tmp2"))
