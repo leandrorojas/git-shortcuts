39fc6a0895b7e92d301309aea867dabb57d6f3e1
Leandro Rojas <lrojas@tecnoazar.com.ar>
2024-10-24 11:02:48 -0300

    "changed reference to common.sh"

--- src/common.sh
+++ src/common.sh
@@ -3,7 +3,7 @@
 readonly MAIN_BRANCH="main" #TODO: read this from .config
 readonly HIDDEN_FILE=".hidden"
 
-function git_branch(){
+function common::git_branch(){
     if [[ $# -eq 1 ]]; then
         git branch "${1}"
     else
@@ -11,20 +11,20 @@ function git_branch(){
     fi
 }
 
-function git_branch_enum(){
+function common::git_branch_enum(){
     git branch | cut -c 3-
 }
 
-function git_checkout(){
+function common::git_checkout(){
     git checkout "${1}"
 }
 
-function git_commit(){
+function common::git_commit(){
     git commit -am \""${1}"\"
 }
 
 # shellcheck disable=SC2120
-function git_push(){
+function common::git_push(){
     if [[ $# -eq 1 ]]; then
         git push -u origin "${1}"
     else
@@ -32,36 +32,36 @@ function git_push(){
     fi
 }
 
-function git_status(){
+function common::git_status(){
     git status
 }
 
-function git_add_all(){
+function common::git_add_all(){
     git add "-A"
 }
 
-function git_stash(){
+function common::git_stash(){
     git stash
 }
 
-function git_rebase(){
+function common::git_rebase(){
     git rebase "${1}"
 }
 
-function git_pull_rebase(){
+function common::git_pull_rebase(){
     git pull --rebase origin "${1}"
 }
 
-function git_reset(){
+function common::git_reset(){
     git reset --hard
 }
 
 #file
-function add_hidden_file(){
+function common::add_hidden_file(){
     git add $HIDDEN_FILE
 }
 
-function file_create_control(){
+function common::file_create_control(){
     if [ ! -f $HIDDEN_FILE ]; then
         file_create
     else
@@ -69,20 +69,20 @@ function file_create_control(){
     fi
 }
 
-function file_create(){
+function common::file_create(){
     echo " " > $HIDDEN_FILE
     add_hidden_file
 }
 
-function file_remove(){
+function common::file_remove(){
     rm $HIDDEN_FILE
 }
 
-function branch_create(){
+function common::branch_create(){
     git_branch "${1}"
 }
 
-function branch_delete_all(){
+function common::branch_delete_all(){
     for to_delete in $(git_branch_enum); do
         if [[ $to_delete != "$MAIN_BRANCH" ]]; then
             git branmch -D "$to_delete"
@@ -92,45 +92,45 @@ function branch_delete_all(){
     git remote prune origin
 }
 
-function get_main(){
+function common::get_main(){
     git_checkout ${MAIN_BRANCH}
     git pull
 }
 
-function tag_copy(){
+function common::tag_copy(){
     git tag "${1}" "${2}"
 }
 
-function tag_delete(){
+function common::tag_delete(){
     git tag -d "${1}"
 }
 
-function tag_push_deleted(){
+function common::tag_push_deleted(){
     git push origin :refs/tags/"${1}"
 }
 
-function tag_push_all(){
+function common::tag_push_all(){
     git push --tags
 }
 
-function increment_send(){
+function common::increment_send(){
     git_commit "${1}"
     git_push
 }
 
-function get_current_branch(){
+function common::get_current_branch(){
     git symbolic-ref --short -q HEAD
 }
 
-function apply_stash(){
+function common::apply_stash(){
     git stash apply
 }
 
-function get_origin_url(){
+function common::get_origin_url(){
     git ls-remote --get-url origin
 }
 
-function fix_branch_fetching(){
+function common::fix_branch_fetching(){
     for origin_url in $(get_origin_url); do
         git remote rm "${origin_url}"
         git remote add "${origin_url}"
